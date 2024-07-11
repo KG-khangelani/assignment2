c66720d0973773cc5f62e15ae93ef21cf5e9b5b4
Kaygizzle <62054578+KG-khangelani@users.noreply.github.com>
2024-07-12 01:29:03 +0200

    cleanup

--- question3.cpp
+++ question3.cpp
@@ -7,7 +7,7 @@ Each line in the file contains the following data: the number of members in a fa
 of family members that ordered the special, the number of bottles of wine that the family ordered,
 and the total bill amount. Create the file orders.dat containing the orders of all the families that 
 Peter served, as specified below. Declare variables of type int for the first three values and a 
-variable of type float for the total bill amount.
+variable of type double for the total bill amount.
 
 read data from the file ordars.dat and display it on the screen.
 orders.dat contains the following data:
@@ -18,6 +18,7 @@ orders.dat contains the following data:
 
 #include <iostream>
 #include <fstream>
+#include <iomanip>
 
 using namespace std;
 
@@ -25,7 +26,7 @@ struct order {
     int family_members;
     int members_ordered_special;
     int wine_bottles;
-    float total_bill;
+    double total_bill;
 };
 
 order orders[9];
@@ -51,15 +52,15 @@ void create_file() {
     outfile.close(); // Close the file
 }
 
-void output_file(int families_ordered_special, float special_meal_commission, float average_spending_per_person) {
-    std::ofstream outfile("result.dat"); // Open the file for writing
+void output_file(int& families_ordered_special, double& special_meal_commission, double& average_spending_per_person) {
+    ofstream outfile("result.dat"); // Open the file for writing
     if (!outfile) {
-        std::cerr << "Unable to create file" << std::endl;
+        std::cerr << "Unable to create file" << endl;
         return;
     }
 
     // Write data to the file
-    outfile << "Displaying constents of result.dat:" << endl << endl;
+    outfile << "Displaying contents of result.dat\n" << endl;
     outfile << "Number of families that ordered the special: " << families_ordered_special << endl;
     outfile << fixed << setprecision(2) << "Commission earned from the special meal: R" << special_meal_commission << endl;
     outfile << fixed << setprecision(2) << "Average spent per person for the evening: R" << average_spending_per_person << endl;
@@ -67,14 +68,14 @@ void output_file(int families_ordered_special, float special_meal_commission, fl
 }
 
 void read_file() {
-    std::ifstream infile("orders.dat"); // Open the file for reading
+    ifstream infile("orders.dat"); // Open the file for reading
     if (!infile) {
-        std::cerr << "Unable to open file" << std::endl;
+        std::cerr << "Unable to open file" << endl;
         return;
     }
 
     int family_members, special_members, wine_bottles;
-    float total_bill;
+    double total_bill;
 
     while (infile >> family_members >> special_members >> wine_bottles >> total_bill) {
         int index;
@@ -90,10 +91,10 @@ void read_file() {
 
 int main() {
     int families_ordered_special = 0;
-    float special_meal_commission;
-    float average_spending_per_person;
+    double special_meal_commission;
+    double average_spending_per_person;
     int total_number_of_people = 0;
-    float total_money_spent = 0;
+    double total_money_spent = 0;
 
     create_file();
     read_file();
@@ -108,7 +109,7 @@ int main() {
     }
 
     // Calculate the average spending per person
-    average_spending_per_person += total_money_spent / static_cast<float>(total_number_of_people);
+    average_spending_per_person += total_money_spent / static_cast<double>(total_number_of_people);
 
     // Output the results to a file
     output_file(families_ordered_special, special_meal_commission, average_spending_per_person);
