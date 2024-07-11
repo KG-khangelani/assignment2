13c00492d0d496d47b3e3abc81c828f3755a53a0
Kaygizzle <62054578+KG-khangelani@users.noreply.github.com>
2024-07-11 22:38:30 +0200

    the vector way

--- question4v2.cpp
+++ question4v2.cpp
@@ -8,14 +8,36 @@ Write a program that reads an input file with the letter character by character.
 #include <iostream>
 #include <fstream>
 #include <cstring>
+#include <vector>
+
 using namespace std;
 
-char content[] = "";
-char encoded_content[] = "";
+vector<char> content = {};
+vector<char> encoded_content = {};
 
-void replaceCharWithString(const char str[], char ch, const char newStr[]) {
-    char result[] = "";
-    char toReplace[] = {ch, ch &= ~' '}; // to replace both upper and lower case
+void replaceCharWithString(vector<char> &full_letter, char char_to_replace, const vector<char>& replacement) {
+    for (int i = 0; i < full_letter.size(); i++) {
+        if (full_letter[i] == char_to_replace) {
+            full_letter.erase(full_letter.begin() + i);
+            for (char j : replacement) {
+                full_letter.insert(full_letter.begin() + i, j);
+                i++;
+            }
+        }
+
+        if (full_letter[i] == toupper(char_to_replace)) {
+            full_letter.erase(full_letter.begin() + i);
+            for (char j: replacement) {
+                full_letter.insert(full_letter.begin() + i, toupper(j));
+                i++;
+            }
+        }
+    }
+
+    for (char i : full_letter) {
+        cout << i;
+    }
+    cout << endl;
 }
 
 void encode_letter () {
@@ -26,12 +48,12 @@ void encode_letter () {
 }
 
 int main () {
-    char letter[] = "Dear Julia, You are the";
-    char encoded_letter[] = "";
+    vector<char> letter = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
+    vector<char> encoded_letter;
     char ch = 'a';
 //    replaceCharWithString(letter, ch, "VV");
     encode_letter();
-    cout << "File has been created and encoded successfully" << endl;
-    replaceCharWithString(letter, ch, "VV");
+    cout << "loading..." << endl;
+    replaceCharWithString(letter, ch, {'V', 'V'});
     return 0;
 }
